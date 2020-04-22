ca70fdd69c923a25e9085e243d91e11c09f02467
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-22 05:24:43 -1000

    add test for adding ingredients and it can show ingredient for recipe

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -21,6 +21,18 @@ class ReceipeTest < MiniTest::Test
     assert_equal ({}), recipe1.ingredients_required
   end
 
+  def test_it_can_add_ingredients
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = ({ingredient1 => 6, ingredient2 => 8})
+    assert_equal expected, recipe1.ingredients_required
+  end
+
+  
 
 
 
